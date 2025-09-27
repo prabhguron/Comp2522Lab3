b5da2a836476c13670d2895a0e90b67cd462a6f1
prabhguron <prabhten@gmail.com>
2025-09-26 21:51:02 -0700

    starting code will push more check by end of staturday for full code

---
+++ src/IDevice.java
@@ -0,0 +1,18 @@
+abstract class IDevice
+{
+    private final String purpose;
+
+    public IDevice(String purpose)
+    {
+        this.purpose = purpose;
+    }
+
+    public String getPurpose(final String purpose)
+    {
+        return purpose;
+    }
+
+     abstract void printDetails();
+
+
+}
---
+++ src/IPod.java
@@ -0,0 +1,24 @@
+public class IPod extends IDevice
+{
+
+    private static final String IPodPurpose = "music";
+    private final int numStoredSongs;
+    private final double maxVolume;
+
+     IPod(final int numStoredSongs,
+          final double maxVolume)
+    {
+        super(IPodPurpose);
+        this.numStoredSongs = numStoredSongs;
+        this.maxVolume = maxVolume;
+    }
+
+    @Override
+    public void printDetails()
+    {
+        System.out.println("IPod purpose: " + IPodPurpose
+                + ", songs stored: " + numStoredSongs
+                + ", max volume: " + maxVolume + " dB");
+    }
+
+}
